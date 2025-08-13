820b8d3cc35bf616fc3d15e3cc3544c773d4b227
Dani Dev <daniel.alexis.b1@gmail.com>
2025-08-13 18:24:09 -0400

    first commit

---
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -0,0 +1,53 @@
+package com.app.todoapp.controller;
+
+
+import com.app.todoapp.model.Task;
+import com.app.todoapp.service.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model){
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+
+    @PostMapping
+    public String createTasks(@RequestParam  String title){
+        taskService.createTask(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTasks(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTasks(@PathVariable Long id){
+        taskService.toggleTask(id);
+        return "redirect:/";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String editTask(@PathVariable Long id, @RequestParam String title) {
+        taskService.editTask(id, title);
+        return "redirect:/"; // Volvemos a la lista de tareas
+    }
+
+}
